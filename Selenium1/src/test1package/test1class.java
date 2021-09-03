22a16adcae7a4df0b36e808dbf11eb4651ce9f0e
prashanth <8rkprashanth@gmail.com>
2021-09-04 01:39:26 +0530

    New project
    Change-Id: Ieda478bcd409bdce2d6b6cdfac4e6a90c17b915b
    Signed-off-by: prashanth <8rkprashanth@gmail.com>

---
+++ Selenium1/src/test1package/test1class.java
@@ -0,0 +1,11 @@
+package test1package;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class test1class {
+	public static void main(String[] args) {
+		WebDriver Driver = new FirefoxDriver();
+		Driver.get("https://apiportaldevsaturnxmjlqwr0ig-ed50aclw58.int.sap.eu2.hana.ondemand.com/shell/develop");
+	}
+}
