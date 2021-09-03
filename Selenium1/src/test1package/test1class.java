dd09434d2d2183750659d3b9a75aa97164df349a
prashanth <8rkprashanth@gmail.com>
2021-09-04 01:55:28 +0530

    rk
    Change-Id: I08cfee2edd95f1024a8f038a920f67571fc72ff8
    Signed-off-by: prashanth <8rkprashanth@gmail.com>

--- Selenium1/src/test1package/test1class.java
+++ Selenium1/src/test1package/test1class.java
@@ -7,6 +7,6 @@ public class test1class {
 	public static void main(String[] args) {
 		WebDriver Driver = new FirefoxDriver();
 		Driver.get("https://apiportaldevsaturnxmjlqwr0ig-ed50aclw58.int.sap.eu2.hana.ondemand.com/shell/develop");
-		System.out.println("hi");
+		System.out.println("hi rk");
 	}
 }
