582b83f7ba77e83cffd9f3295ec0c72529e1acc8
prashanth <8rkprashanth@gmail.com>
2021-09-07 07:39:08 +0530

    maven sel test
    Change-Id: I10210243bdd3f306a25789d9a71b8e83878332fa
    Signed-off-by: prashanth <8rkprashanth@gmail.com>

--- Selenium1/src/test1package/test1class.java
+++ Selenium1/src/test1package/test1class.java
@@ -1,11 +1,19 @@
 package test1package;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class test1class {
 	public static void main(String[] args) {
+		//String projectLocation = System.getProperty("user.dir");
+		//System.out.println(projectLocation);
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\I066337\\Desktop\\Prapthi\\PROJECTS\\Selenium\\geckodriver-v0.29.1-win64");
+		//WebDriver Chrome = new ChromeDriver();
 		WebDriver Driver = new FirefoxDriver();
+		//Chrome.get("https://apiportaldevsaturnxmjlqwr0ig-ed50aclw58.int.sap.eu2.hana.ondemand.com/shell/develop");
+		
 		Driver.get("https://apiportaldevsaturnxmjlqwr0ig-ed50aclw58.int.sap.eu2.hana.ondemand.com/shell/develop");
 		System.out.println("hi rk");
 	}
---
+++ SeleniumTests/src/test/java/LoginSelMav.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginSelMav {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://google.com");
+		driver.close();
+		driver.quit();
+
+	}
+
+}
